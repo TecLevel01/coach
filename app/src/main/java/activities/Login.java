5cf685a5e5b1bd3ef98c2ef4fe7a98122cb00157
TecLevel01 <olivierlubunga@gmail.com>
2022-10-07 17:08:39 +0300

    For login

--- app/src/main/java/activities/Login.java
+++ app/src/main/java/activities/Login.java
@@ -3,14 +3,32 @@ package activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.bdm.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Login extends AppCompatActivity {
+    EditText username, password;
+    Button signIn;
+    FirebaseAuth mAuth;
+    FirebaseFirestore myAuth;
+    TextView signTxt;
+    ProgressBar myProBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.PwTxt);
+        signIn = findViewById(R.id.loginBtn);
+        signTxt = findViewById(R.id.loginTxt);
+        myProBar = findViewById(R.id.progressBar);
+
     }
 }
